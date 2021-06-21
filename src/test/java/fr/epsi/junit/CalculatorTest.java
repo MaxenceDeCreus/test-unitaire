fcdd1739c13bca5ea9674eff50a899e33a417d06
Maxence De Creus <44834400+MaxenceDeCreus@users.noreply.github.com>
2021-06-21 12:08:08 +0200

    Test to class calculator

--- src/test/java/fr/epsi/junit/CalculatorTest.java
+++ src/test/java/fr/epsi/junit/CalculatorTest.java
@@ -1,22 +1,31 @@
 package fr.epsi.junit;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class CalculatorTest {
 
     private Calculator calculator;
 
-    public void init(){
-        //TODO
+    @BeforeAll
+    public static void init(){
+        System.out.println("======================");
+        System.out.println("Init globale");
     }
 
+    @Test
     public void should_add_two_numbers(){
-       // TODO
+        int a = 5;
+        int b = 6;
+        int result = a + b;
+        Assertions.assertEquals(result, Calculator.add(a, b));
     }
 
+    @Test
     public void should_substract_two_numbers(){
-        // TODO
+        int a = 8;
+        int b = 6;
+        int result = a - b;
+        Assertions.assertEquals(result, Calculator.substract(a, b));
     }
 
 
